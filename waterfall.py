68b94adf89ffb0f38c96f31c427270458218de76
EFLin <79702669+EF-Lin@users.noreply.github.com>
2026-07-29 15:34:18 +0800

    Add waterfall.py that can generate waterfall plot

---
+++ waterfall.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.dates import date2num, DateFormatter
+from obspy import Stream, UTCDateTime, read
+from typing import Optional
+
+
+class Water:
+    def __init__(
+            self,
+            st: Stream,
+            frequency: Optional[tuple[int, int]] = None,
+            range: Optional[tuple[UTCDateTime, UTCDateTime]] = None,
+            y_factor: Optional[int] = 20
+    ):
+        self.st: Stream = st
+        self.frequency: tuple = frequency
+        self.y_factor = y_factor
+        if range != None:
+            self.range: tuple = range
+        else:
+            self.range = (max(tr.stats.starttime for tr in st), min(tr.stats.endtime for tr in st))
+
+    def process(self):
+        st.detrend("demean")
+        st.detrend("linear")
+        st.filter("bandpass", freqmin=1.0, freqmax=10.0)
+        st.normalize()
+        # self.data = np.array([tr.data for tr in st])
+
+    def cut(self):
+        st.trim(starttime=self.range[0], endtime=self.range[1])
+
+    def waterfall_plot(self):
+        self.data = np.array([tr.data for tr in st])
+        _, ax = plt.subplots(figsize=(12, 6))
+
+        im = ax.imshow(
+            self.data,
+            aspect="auto",
+            cmap="seismic",
+            origin="upper",
+            extent=[date2num(self.range[0].datetime), date2num(self.range[1].datetime), self.data.shape[0]-0.5, -0.5]
+        )
+
+        if self.frequency != None:
+            im = ax.imshow(vmin=self.frequency(0), vmax=self.frequency(1))
+
+        # color bar
+        cbar = plt.colorbar(im, ax=ax)
+        cbar.set_label("Amplitude")
+
+        # Y
+        trace_ids = [tr.id for tr in st]
+        n_traces = len(trace_ids)
+        step = n_traces // self.y_factor
+
+        ax.set_yticks(range(0, n_traces, step))
+        ax.set_yticklabels([trace_ids[i] for i in range(0, n_traces, step)])
+        ax.set_ylabel("Trace")
+
+        # X
+        ax.set_xlabel("Time (UTC)")
+        ax.xaxis_date()
+        ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d %H:%M:%S'))
+
+        ax.set_title(f"{self.st[0].id} to {self.st[0-1].id} Waterfall Plot")
+        plt.setp(ax.xaxis.get_majorticklabels(), rotation=10, ha="center")
+        plt.tight_layout()
+        plt.show()
+
+
+if __name__ == "__main__":
+    st = read("t/20230403-000021.mseed")
+    wa = Water(st)
+    wa.process()
+    wa.cut()
+    wa.waterfall_plot()
