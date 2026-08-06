5cb739593d5dff592a128dfe6f32dff392011232
EFLin <79702669+EF-Lin@users.noreply.github.com>
2026-08-06 16:51:22 +0800

    add tqdm, close Porgress Bar Fixes #1

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
-from src.load.loadTR import load_mini, transfer
-from src.load.loadCSV import load_csv
-from src.plot.finger import Finger
-from src.load.search import Search
-from src.plot.waterfall import Water, Fall
+from src.load import load_mini, transfer, load_csv, Search
+from src.plot import Finger, Water, Fall
+from src.utils import timer
 import numpy as np
+from tqdm import tqdm
 
 
+@timer
+def main():
+    se = Search("data_100Hz")
+    li = load_csv("asset/78.csv")[50:]
 
-se = Search("data_100Hz")
-li = load_csv("asset/78.csv")
+    st = load_mini(se.find(1334))
+    fi = Finger(st)
+    fi.process()
 
-"""
-st = load_mini(se.find(1334))
-fi = Finger(st)
-fi.process()
-"""
+    st = load_mini(se.multi_find(1208, 170))
+    pa, stations = transfer(st)
 
-st = load_mini(se.multi_find(1208, 170))
-pa, stations = transfer(st)
-wa = Water(pa, range=(np.datetime64("2026-07-08T14:27:05"), 30))
-wa.cut()
-pa2 = wa.process()
-fa = Fall(pa2)
-fa.waterfall_plot()
+    for i in tqdm(li):
+        t = np.datetime64(i["Time"])
+        name = f"{stations[0]}_to_{stations[1]}_waterfall_plot_{i["Time"].replace(':', '')}"
 
-"""
-for i in li:
-    t = UTCDateTime(i["Time"])
-    #fi.focus_save(t)
+        fi.focus_save(i["Time"])
 
-    fa = Fall(st=st2.copy(), y_factor=20, range=(t, 20))
-    fa.cut()
-    fa.waterfall_save()
-"""
+        wa = Water(pa, range=(t, 20))
+        pa2 = wa.process(wa.cut())
+        fa = Fall(pa2, name=name)
+        fa.waterfall_save()
 
-# f"{self.pa[0].id}_to_{self.st[0-1].id}_waterfall_plot_{self.range[0].strftime("%Y-%m-%dT%H%M%S")}"
+
+if __name__ == "__main__":
+    main()
